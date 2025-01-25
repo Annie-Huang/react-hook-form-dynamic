f808e7c7d1f3690f0e35aa43e16d0813d45beb2c
Annie Huang <annie.hn@gmail.com>
2025-01-25 14:19:05 +1100

    Clean up and copy the initial template of App.tsx in

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='bg-gray-100 min-h-screen'>
+      <div className='max-w-xl w-full mx-auto px-4 py-32'>
+        <form className='bg-white rounded-lg px-8 py-12 shadow-lg'>
+          <h1 className='text-orange-600 text-xl font-semibold mb-8'>
+            Order Package
+          </h1>
+          <label className='block'>
+            <span className='block mb-1 text-gray-600'>Your name</span>
+            <input
+              className='w-full rounded-lg border-gray-300 focus:ring-0 focus:border-orange-600 py-3'
+              type='text'
+            />
+          </label>
+          <label className='block mt-6'>
+            <input
+              type='checkbox'
+              className='text-orange-600 w-5 h-5 border-gray-300 focus:ring-0 focus:ring-offset-0'
+            />
+            <span className='ml-2 text-gray-600'>Send receipt to email?</span>
+          </label>
+
+          <label className='block mt-6'>
+            <span className='block mb-1 text-gray-600'>Email</span>
+            <input
+              className='w-full rounded-lg border-gray-300 focus:ring-0 focus:border-orange-600  py-3'
+              type='text'
+            />
+          </label>
+
+          <button className='mt-12 w-full bg-orange-600 rounded-lg px-6 py-3 text-white font-medium'>
+            Order Package
+          </button>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
