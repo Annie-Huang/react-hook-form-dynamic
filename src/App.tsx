457638ba546b94e5e6d847c345240191ae40181d
Annie Huang <annie.hn@gmail.com>
2025-01-25 15:59:26 +1100

    Add register and JSON.stringify out the value in screen.

--- src/App.tsx
+++ src/App.tsx
@@ -38,12 +38,14 @@ const App: FC = () => {
             <input
               className='w-full rounded-lg border-gray-300 border focus:ring-0 focus:border-orange-600 py-3'
               type='text'
+              {...register('name')}
             />
           </label>
-          <label className='block mt-6'>
+          <label className='block mt-6 flex items-center'>
             <input
               type='checkbox'
               className='text-orange-600 w-5 h-5 border-gray-300 focus:ring-0 focus:ring-offset-0 hover:cursor-pointer'
+              {...register('sendToEMail')}
             />
             <span className='ml-2 text-gray-600 hover:cursor-pointer'>
               Send receipt to email?
@@ -55,8 +57,11 @@ const App: FC = () => {
             <input
               className='w-full rounded-lg border-gray-300 border focus:ring-0 focus:border-orange-600 py-3'
               type='text'
+              {...register('email')}
             />
           </label>
+          {/* You can tick and untick checkbox to see the value change */}
+          <pre>{JSON.stringify(watch(), null, 2)}</pre>
 
           <button className='mt-12 w-full bg-orange-600 rounded-lg px-6 py-3 text-white font-medium hover:cursor-pointer'>
             Order Package
