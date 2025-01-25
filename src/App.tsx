a8c51375346d4b2ed62b4ddcacb890e4d1e614a9
Annie Huang <annie.hn@gmail.com>
2025-01-25 14:55:39 +1100

    Fixed App.tsx to make border of the <input> appear and add pointer on hover

--- src/App.tsx
+++ src/App.tsx
@@ -9,27 +9,29 @@ function App() {
           <label className='block'>
             <span className='block mb-1 text-gray-600'>Your name</span>
             <input
-              className='w-full rounded-lg border-gray-300 focus:ring-0 focus:border-orange-600 py-3'
+              className='w-full rounded-lg border-gray-300 border focus:ring-0 focus:border-orange-600 py-3'
               type='text'
             />
           </label>
           <label className='block mt-6'>
             <input
               type='checkbox'
-              className='text-orange-600 w-5 h-5 border-gray-300 focus:ring-0 focus:ring-offset-0'
+              className='text-orange-600 w-5 h-5 border-gray-300 focus:ring-0 focus:ring-offset-0 hover:cursor-pointer'
             />
-            <span className='ml-2 text-gray-600'>Send receipt to email?</span>
+            <span className='ml-2 text-gray-600 hover:cursor-pointer'>
+              Send receipt to email?
+            </span>
           </label>
 
           <label className='block mt-6'>
             <span className='block mb-1 text-gray-600'>Email</span>
             <input
-              className='w-full rounded-lg border-gray-300 focus:ring-0 focus:border-orange-600  py-3'
+              className='w-full rounded-lg border-gray-300 border focus:ring-0 focus:border-orange-600 py-3'
               type='text'
             />
           </label>
 
-          <button className='mt-12 w-full bg-orange-600 rounded-lg px-6 py-3 text-white font-medium'>
+          <button className='mt-12 w-full bg-orange-600 rounded-lg px-6 py-3 text-white font-medium hover:cursor-pointer'>
             Order Package
           </button>
         </form>
